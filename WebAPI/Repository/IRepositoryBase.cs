6ea38806560b143c25c32d84537fb4aa0ec8b02b
Jonnas <jonnasmacaraeg@gmail.com>
2023-09-16 11:29:31 +0800

    Added Comments on Repository wrapper and baseRepository

--- WebAPI/Repository/IRepositoryBase.cs
+++ WebAPI/Repository/IRepositoryBase.cs
@@ -2,12 +2,41 @@
 
 namespace WebAPI.Repository
 {
+    /// <summary>
+    /// Represents a base repository interface for CRUD operations on entities of type T.
+    /// </summary>
+    /// <typeparam name="T">The type of entity.</typeparam>
     public interface IRepositoryBase<T>
     {
+        /// <summary>
+        /// Retrieves all entities of type T.
+        /// </summary>
+        /// <returns>An IQueryable of T containing all entities.</returns>
         IQueryable<T> FindAll();
+
+        /// <summary>
+        /// Retrieves entities of type T based on a specified condition.
+        /// </summary>
+        /// <param name="expression">The expression representing the condition.</param>
+        /// <returns>An IQueryable of T containing entities that match the condition.</returns>
         IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression);
+
+        /// <summary>
+        /// Creates a new entity of type T.
+        /// </summary>
+        /// <param name="entity">The entity to create.</param>
         void Create(T entity);
+
+        /// <summary>
+        /// Updates an existing entity of type T.
+        /// </summary>
+        /// <param name="entity">The entity to update.</param>
         void Update(T entity);
+
+        /// <summary>
+        /// Deletes an entity of type T.
+        /// </summary>
+        /// <param name="entity">The entity to delete.</param>
         void Delete(T enitity);
     }
 }
--- WebAPI/Repository/IRepositoryWrapper.cs
+++ WebAPI/Repository/IRepositoryWrapper.cs
@@ -4,11 +4,20 @@ using WebAPI.Repository.Transaction;
 
 namespace WebAPI.Repository
 {
+    /// <summary>
+    /// Provides a wrapper interface for accessing various repositories and the application context.
+    /// </summary>
     public interface IRepositoryWrapper
     {
         IAccountRepository Account {  get; }
         ITransactionRepository Transaction { get; }
+        /// <summary>
+        /// Gets the application context.
+        /// </summary>
         ApplicationContext GetContext();
+        /// <summary>
+        /// Asynchronously saves changes made to the repositories.
+        /// </summary>
         Task SaveAsync();
     }
 }
